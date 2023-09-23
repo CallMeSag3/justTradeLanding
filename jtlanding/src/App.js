aa5299fd45afd130405c985d2b60ae0eed0bbbdf
sage <antoshkakorol301@gmail.com>
2023-09-23 11:57:19 -0500

    topbar start

--- jtlanding/src/App.js
+++ jtlanding/src/App.js
@@ -1,7 +1,18 @@
 import "./App.scss";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TopBar from "./components/topBar/TopBar";
 
 function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
