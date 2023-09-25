fcac4f1eb157230959f2333d9a8a0ac26c72083e
sage <antoshkakorol301@gmail.com>
2023-09-24 20:01:20 -0500

    strengths + info + gains

---
+++ jtlanding/src/components/gains/Gains.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./gains.scss";
+
+const Gains = () => {
+    return (
+        <div className="gains">
+
+        </div>
+    )
+}
+
+export default Gains;
--- jtlanding/src/pages/Home.jsx
+++ jtlanding/src/pages/Home.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./home.scss";
 import Header from "../components/header/Header"
 import Benefits from "../components/benefits/Benefits"
+import Strengths from "../components/strengths/Strengths"
+import Information from "../components/information/Information"
+import Gains from "../components/gains/Gains"
 
 const Home = () => {
     return (
         <div className="home">
             <Header/>
             <Benefits/>
+            <Strengths/>
+            <Information/>
+            <Gains/>
         </div>
     )
 }
