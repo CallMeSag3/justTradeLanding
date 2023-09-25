23d6234dcc687c3c781820394f14f4ee893a39f9
sage <antoshkakorol301@gmail.com>
2023-09-25 12:34:17 -0500

    team slider

--- jtlanding/src/components/team/Team.jsx
+++ jtlanding/src/components/team/Team.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./team.scss";
+import Slider from "react-slick";
+import Person from '../../images/person.png'
+import User from '../../images/user.png'
+import Chat from '../../images/chat.png'
+import Calendar from '../../images/calendar.png'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Team = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+    }
     return (
         <div className="team">
             <div className="team__container">
@@ -13,7 +30,78 @@ const Team = () => {
                         Con nuestro equipo de profesionales podra llegar a las metas de negocio que tienen de manera rapida y efectiva
                     </p>
                 </div>
-
+                <Slider {...settings} className="team__slider">
+                    <div className="team__slider-slide">
+                        <img src={Person} alt="Slide" className="team__slider-slide-img" />
+                        <div className="team__slider-slide-info">
+                            <div className="team__slider-slide-textcont">
+                                <h3 className="team__slider-slide-name">Anton Patrignani</h3>
+                                <p className="team__slider-slide-role">CEO</p>
+                            </div>
+                            <div className="team__slider-slide-contact">
+                                <img src={Calendar} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={User} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={Chat} alt="contact link" className="team__slider-slide-contact-img" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="team__slider-slide">
+                        <img src={Person} alt="Slide" className="team__slider-slide-img" />
+                        <div className="team__slider-slide-info">
+                            <div className="team__slider-slide-textcont">
+                                <h3 className="team__slider-slide-name">Anton Patrignani</h3>
+                                <p className="team__slider-slide-role">CEO</p>
+                            </div>
+                            <div className="team__slider-slide-contact">
+                                <img src={Calendar} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={User} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={Chat} alt="contact link" className="team__slider-slide-contact-img" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="team__slider-slide">
+                        <img src={Person} alt="Slide" className="team__slider-slide-img" />
+                        <div className="team__slider-slide-info">
+                            <div className="team__slider-slide-textcont">
+                                <h3 className="team__slider-slide-name">Anton Patrignani</h3>
+                                <p className="team__slider-slide-role">CEO</p>
+                            </div>
+                            <div className="team__slider-slide-contact">
+                                <img src={Calendar} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={User} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={Chat} alt="contact link" className="team__slider-slide-contact-img" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="team__slider-slide">
+                        <img src={Person} alt="Slide" className="team__slider-slide-img" />
+                        <div className="team__slider-slide-info">
+                            <div className="team__slider-slide-textcont">
+                                <h3 className="team__slider-slide-name">Anton Patrignani</h3>
+                                <p className="team__slider-slide-role">CEO</p>
+                            </div>
+                            <div className="team__slider-slide-contact">
+                                <img src={Calendar} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={User} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={Chat} alt="contact link" className="team__slider-slide-contact-img" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="team__slider-slide">
+                        <img src={Person} alt="Slide" className="team__slider-slide-img" />
+                        <div className="team__slider-slide-info">
+                            <div className="team__slider-slide-textcont">
+                                <h3 className="team__slider-slide-name">Anton Patrignani</h3>
+                                <p className="team__slider-slide-role">CEO</p>
+                            </div>
+                            <div className="team__slider-slide-contact">
+                                <img src={Calendar} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={User} alt="contact link" className="team__slider-slide-contact-img" />
+                                <img src={Chat} alt="contact link" className="team__slider-slide-contact-img" />
+                            </div>
+                        </div>
+                    </div>
+                </Slider>
             </div>
         </div>
     )
